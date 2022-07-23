fea7d999a91d364dd65633b3a056ffceb1ec1527
barry <barry@DESKTOP-FE66TFB.hitronhub.home>
2022-07-22 21:17:29 -0700

    Test object initialization.

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Spring framwork", "Spring Framwork Description"),
+				new Topic("java", "Core Java", "Core Java Description"),
+				new Topic("javascript", "Javascript", "Javascript Description"));
 	}
 
 }
