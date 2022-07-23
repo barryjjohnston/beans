9c44992b6ca34e4bcb1215cd7cd23ca122e5a6bd
barry <barry@DESKTOP-FE66TFB.hitronhub.home>
2022-07-22 18:24:43 -0700

    Created main class

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootstarter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
